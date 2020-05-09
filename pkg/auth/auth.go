9d5d8889f97b2738007c6b00b7819b5f3845ff50
shanedabes <shane@donohoe.cc>
2020-05-09 16:54:09 +0100

    Add coverage tests for load token function
    Add more tests to cover errors returned by the load token function

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// LoadToken loads a token from cache using an io.Reader
 func LoadToken(r io.Reader) (t oauth2.Token, err error) {
 	j, err := ioutil.ReadAll(r)
 	if err != nil {
--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
@@ -17,4 +18,22 @@ func TestLoadToken(t *testing.T) {
 
 	_, err := LoadToken(r)
 	assert.Nil(t, err)
+}
+
+type errReader struct{}
+
+func (e errReader) Read([]byte) (int, error) {
+	return 0, errors.New("test error")
+}
+
+func TestLoadTokenReadErr(t *testing.T) {
+	_, err := LoadToken(errReader{})
+	assert.NotNil(t, err)
+}
+
+func TestLoadTokenJSONErr(t *testing.T) {
+	r := strings.NewReader("")
+	_, err := LoadToken(r)
+
+	assert.NotNil(t, err)
 }
