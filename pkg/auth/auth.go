64b858f2ac2116a10d9a1bb64601be3a89c0a60b
shanedabes <shane@donohoe.cc>
2020-05-09 18:32:14 +0100

    Add function to save token

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -2,13 +2,14 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 
 	"golang.org/x/oauth2"
 )
 
-// LoadToken loads a token from cache using an io.Reader
+// LoadToken loads a token from cache file (or any io.Reader)
 func LoadToken(r io.Reader) (t oauth2.Token, err error) {
 	j, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -21,4 +22,19 @@ func LoadToken(r io.Reader) (t oauth2.Token, err error) {
 	}
 
 	return t, nil
+}
+
+// SaveToken writes a token (or any json) to a file (or any io.Writer)
+func SaveToken(i interface{}, w io.Writer) (err error) {
+	j, err := json.MarshalIndent(i, "", "  ")
+	if err != nil {
+		return
+	}
+
+	_, err = fmt.Fprint(w, string(j)+"\n")
+	if err != nil {
+		return
+	}
+
+	return
 }
--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -1,11 +1,15 @@
 package auth
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
+	"io/ioutil"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
 )
 
 func TestLoadToken(t *testing.T) {
@@ -20,14 +24,18 @@ func TestLoadToken(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-type errReader struct{}
+type errReadWriter struct{}
 
-func (e errReader) Read([]byte) (int, error) {
+func (e errReadWriter) Read([]byte) (int, error) {
+	return 0, errors.New("test error")
+}
+
+func (e errReadWriter) Write([]byte) (int, error) {
 	return 0, errors.New("test error")
 }
 
 func TestLoadTokenReadErr(t *testing.T) {
-	_, err := LoadToken(errReader{})
+	_, err := LoadToken(errReadWriter{})
 	assert.NotNil(t, err)
 }
 
@@ -35,5 +43,40 @@ func TestLoadTokenJSONErr(t *testing.T) {
 	r := strings.NewReader("")
 	_, err := LoadToken(r)
 
+	assert.NotNil(t, err)
+}
+
+func TestSaveToken(t *testing.T) {
+	buf := &bytes.Buffer{}
+	token := oauth2.Token{}
+
+	err := SaveToken(token, buf)
+	assert.Nil(t, err)
+
+	j, err := ioutil.ReadFile("testdata/token.json")
+	assert.Nil(t, err)
+	assert.Equal(t, string(j), buf.String())
+}
+
+type errJSON struct{}
+
+func (e errJSON) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("Marshal error")
+}
+
+func TestSaveTokenJsonErr(t *testing.T) {
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	token := errJSON{}
+
+	err := SaveToken(&token, w)
+	assert.NotNil(t, err)
+}
+
+func TestSaveTokenWriteErr(t *testing.T) {
+	token := oauth2.Token{}
+	w := errReadWriter{}
+
+	err := SaveToken(&token, w)
 	assert.NotNil(t, err)
 }
